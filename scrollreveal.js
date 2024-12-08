d155dd7ca9aee67c055213d842d2e793d20dc921
FR4NC31 <129973535+FR4NC31@users.noreply.github.com>
2024-12-08 11:25:03 +0800

    first commit

---
+++ scrollreveal.js
@@ -0,0 +1,99 @@
+ScrollReveal().reveal('.navbar ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.container_1 .context  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    origin: 'left',
+});
+
+ScrollReveal().reveal('.container_1 .imgs  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    origin: 'right',
+});
+
+ScrollReveal().reveal('.container_2 h5  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    origin: 'left',
+});
+ScrollReveal().reveal('.container_2 button  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    origin: 'left',
+});
+ScrollReveal().reveal('.container_2 .imgs1  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1800,
+    scale: .7,
+    duration: 950,
+});
+ScrollReveal().reveal('.container_2 .imgs2  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1600,
+    scale: .7,
+    duration: 850,
+});
+ScrollReveal().reveal('.container_2 .imgs3  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1400,
+    scale: .7,
+    duration: 750,
+});
+ScrollReveal().reveal('.container_2 .imgs4  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1200,
+    scale: .7,
+    duration: 650,
+});
+ScrollReveal().reveal('.container_2 .imgs5  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    scale: .7,
+    duration: 500,
+});
+
+ScrollReveal().reveal('.container_3 .imgs  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    origin: 'left',
+});
+ScrollReveal().reveal('.container_3 h5  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    origin: 'right',
+});
+ScrollReveal().reveal('.container_3 button  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    origin: 'right',
+});
+
+ScrollReveal().reveal('.container_4 .signupForm  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    origin: 'right',
+});
+ScrollReveal().reveal('.container_4 .sign  ', {
+    opacity: 0,
+    distance: '50px',
+    duration: 1000,
+    origin: 'left',
+});
